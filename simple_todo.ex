9ed4a2ff71a174c62b2df72fbf6a7207baa4f20a
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-25 12:23:27 -0400

    add basic crud to todo list

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,15 +1,49 @@
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct next_id: 1, entries: %{}
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      fn entry, todo_list_acc ->
+        add_entry(todo_list_acc, entry)
+      end
+    )
   end
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+    entry = Map.put(entry, :id, todo_list.next_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.next_id,
+        entry
+      )
+
+    %TodoList{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
   end
 
-  def entires(todo_list, date) do
-    MultiDict.get(todo_list, date)
+  def update_entry(todo_list, entry_id, updater_func) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_func.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(&(&1.date == date))
   end
 end
