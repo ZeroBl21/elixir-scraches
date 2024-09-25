f46c7f68167a1ba5ad19b3233d26e4dfd6ab644f
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-25 12:43:17 -0400

    add some ultra basic 'CSV' importer

--- simple_todo.ex
+++ simple_todo.ex
@@ -47,3 +47,27 @@ defmodule TodoList do
     |> Enum.filter(&(&1.date == date))
   end
 end
+
+defmodule TodoList.CSVImporter do
+  def import(path) do
+    path
+    |> read_lines()
+    |> create_entries()
+    |> TodoList.new()
+  end
+
+  defp read_lines(path) do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+  end
+
+  defp create_entries(path) do
+    path
+    |> Stream.map(fn line ->
+      [date_string, title] = String.split(line, ",")
+      date = Date.from_iso8601!(date_string)
+      %{date: date, title: title}
+    end)
+  end
+end
