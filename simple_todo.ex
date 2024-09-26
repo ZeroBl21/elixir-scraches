a4bb776b743f91596da5f2f3aa5bc51f8cdd3fc2
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-26 14:57:13 -0400

    feat(simple_todo.ex): Add Collectable and String.Chars implementation

--- simple_todo.ex
+++ simple_todo.ex
@@ -70,4 +70,25 @@ defmodule TodoList.CSVImporter do
       %{date: date, title: title}
     end)
   end
+
+  # Implementations
+
+  defimpl String.Chars, for: TodoList do
+    def to_string(_) do
+      "#TodoList"
+    end
+  end
+
+  defimpl Collectable, for: TodoList do
+    def into(original) do
+      {original, &into_callback/2}
+    end
+
+    defp into_callback(todo_list, {:cont, entry}) do
+      TodoList.add_entry(todo_list, entry)
+    end
+
+    defp into_callback(todo_list, :done), do: todo_list
+    defp into_callback(_todo_list, :halt), do: :ok
+  end
 end
