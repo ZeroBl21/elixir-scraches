c78b98efd5ff72ef7a48372440420bb09e38e3f7
Darwin Perdomo <ZeroBl@hotmail.com>
2024-10-04 15:26:30 -0400

    feat(ch9/dynamic_workers): Add Dynamic Supervisor to Todo.Cache

---
+++ ch9/dynamic_workers/lib/todo/system.ex
@@ -0,0 +1,13 @@
+defmodule Todo.System do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link([Todo.ProcessRegistry, Todo.Database, Todo.Cache],
+      strategy: :one_for_one
+    )
+  end
+
+  def init(_) do
+    Supervisor.init([Todo.Cache], strategy: :one_for_one)
+  end
+end
