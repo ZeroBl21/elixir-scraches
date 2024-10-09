2cf3683902bb0428324ce23486c5b98cc7a88f38
Darwin Perdomo <ZeroBl@hotmail.com>
2024-10-09 13:13:21 -0400

    feat(ch11/todo_app): Add poolboy implementation

--- ch11/todo_app/lib/todo/database.ex
+++ ch11/todo_app/lib/todo/database.ex
@@ -11,19 +11,21 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.store(key, data)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.store(worker_pid, key, data)
+      end
+    )
   end
 
   def get(key) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.get(key)
-  end
-
-  defp choose_worker(key) do
-    :erlang.phash2(key, @pool_size) + 1
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.get(worker_pid, key)
+      end
+    )
   end
 
   def handle_call({:choose_worker, key}, _, workers) do
@@ -32,11 +34,17 @@ defmodule Todo.Database do
   end
 
   def child_spec(_) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
+    File.mkdir_p!(@db_folder)
+
+    :poolboy.child_spec(
+      __MODULE__,
+      [
+        name: {:local, __MODULE__},
+        worker_module: Todo.DatabaseWorker,
+        size: @pool_size
+      ],
+      [@db_folder]
+    )
   end
 
   defp worker_spec(worker_id) do
