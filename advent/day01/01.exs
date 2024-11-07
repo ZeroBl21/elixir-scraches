edfda9e6f08349ed83d85db7f57a639be4643903
Darwin Perdomo <ZeroBl@hotmail.com>
2024-11-07 17:27:54 -0400

    feat(advent/day01): Add the solutions together

--- advent/day01/01.exs
+++ advent/day01/01.exs
@@ -1,26 +1,47 @@
-defmodule Day1 do
-  def start() do
-    "./input.txt"
+defmodule Day01 do
+  @input "./input.txt"
+
+  def part1() do
+    @input
+    |> get_input()
+    |> Stream.map(&fuel_required/1)
+    |> Enum.sum()
+    |> IO.inspect(label: "Total Fuel for Part 1")
+  end
+
+  def part2() do
+    @input
+    |> get_input()
+    |> Stream.map(&total_fuel_per_module/1)
+    |> Enum.sum()
+    |> IO.inspect(label: "Total Fuel for Part 2")
+  end
+
+  defp get_input(path) do
+    path
     |> File.stream!()
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.to_integer/1)
-    |> Stream.map(&total_fuel_per_module/1)
-    |> Enum.sum()
-    |> IO.inspect(label: "Total Fuel")
   end
 
-  defp total_fuel_per_module(mass), do: fuel_for_mass(mass, 0)
+  defp fuel_required(mass) do
+    mass
+    |> Integer.floor_div(3)
+    |> Kernel.-(2)
+    |> max(0)
+  end
+
+  defp total_fuel_per_module(mass) do
+    fuel_for_mass(mass, 0)
+  end
 
   defp fuel_for_mass(mass, acc) when mass <= 0, do: acc
 
   defp fuel_for_mass(mass, acc) do
-    fuel =
-      mass
-      |> Integer.floor_div(3)
-      |> Kernel.-(2)
-
-    fuel_for_mass(max(fuel, 0), acc + max(fuel, 0))
+    fuel = fuel_required(mass)
+    fuel_for_mass(fuel, acc + fuel)
   end
 end
 
-Day1.start()
+Day01.part1()
+Day01.part2()
