ff85b2903c7dbe3bf8814c3a54b6747ab8db1bf4
Darwin Perdomo <ZeroBl@hotmail.com>
2025-04-13 22:43:19 -0400

    feat(concurrent): trying to remember GenServer

---
+++ concurrent/sender/.iex.exs
@@ -0,0 +1,7 @@
+emails = [
+  "hello@world.com",
+  "hola@mundo.com",
+  "nihao@shìjiè.com",
+  "konnichiwa@sekai.com",
+  "zero@zero.com"
+]
---
+++ concurrent/sender/lib/sender.ex
@@ -0,0 +1,36 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sender.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def send_email("konnichiwa@sekai.com" = _email),
+    do: :error
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_send"}
+  end
+
+  @spec notify_all(emails :: list(String.t())) :: any()
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
+
+  #
+end
