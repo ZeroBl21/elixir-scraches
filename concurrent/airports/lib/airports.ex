33fc6a99e2ff9207189ac6f52d7624b7648f2d46
Darwin Perdomo <ZeroBl@hotmail.com>
2025-04-19 15:34:58 -0400

    feat(concurrent/airports/): Adding Flow.on_trigger

--- concurrent/airports/lib/airports.ex
+++ concurrent/airports/lib/airports.ex
@@ -6,8 +6,14 @@ defmodule Airports do
   end
 
   def open_airports() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
     airports_csv()
     |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
     |> Flow.from_enumerable()
     |> Flow.map(fn row ->
       [row] = CSV.parse_string(row, skip_headers: false)
@@ -20,12 +26,25 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window, key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        # Show progress in IEx
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done ->
+          {events, acc}
+
+        {:every, 1000} ->
+          {[], acc}
+      end
+    end)
     |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
     |> Enum.take(10)
-    |> List.flatten()
   end
 
   #
